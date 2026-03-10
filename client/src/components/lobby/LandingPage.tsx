6924e8be00169c9944a00920422012e3a9c0d9fc
Jackson <jackconnerdoyle@gmail.com>
2026-03-10 15:42:33 -0700

    Created basic landing page

---
+++ client/src/components/lobby/LandingPage.tsx
@@ -0,0 +1,68 @@
+import { type ChangeEvent, type SubmitEvent, useState } from "react";
+
+function LandingPage() {
+  const [name, setName] = useState("");
+  const [code, setCode] = useState("");
+
+  const handleCreate = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Call api.createRoom(name) from here
+    alert("Create room");
+  };
+
+  const handleJoin = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert("Join room");
+  };
+
+  return (
+    <div className="flex flex-col h-screen items-center justify-center gap-12">
+      <label className="h-16 flex flex-col mt-6">
+        Name
+        <input
+          type="text"
+          id="nameInput"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="border p-2"
+        />
+      </label>
+      <div className="h-full w-full flex items-center justify-center gap-16 mb-8 px-6">
+        <div className="flex flex-col items-center justify-center h-full w-full border text-center">
+          <form id="createForm" onSubmit={handleCreate}>
+            <h1 className="text-xl text-blue-500 font-bold m-4">
+              Create a new room
+            </h1>
+            <button
+              type="submit"
+              className="border rounded-md p-2 cursor-pointer"
+            >
+              Create Room
+            </button>
+          </form>
+        </div>
+        <div className="flex flex-col items-center justify-center h-full w-full border text-center">
+          <form id="joinForm" onSubmit={handleJoin} className="flex flex-col">
+            <h1 className="text-xl text-red-500 font-bold m-4">Join a room</h1>
+            <input
+              type="text"
+              id="codeInput"
+              placeholder="Room code..."
+              value={code}
+              onChange={(e) => setCode(e.target.value)}
+              className="border p-2 mb-4"
+            />
+            <button
+              type="submit"
+              className="border rounded-md p-2 cursor-pointer"
+            >
+              Join Room
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { LandingPage };
